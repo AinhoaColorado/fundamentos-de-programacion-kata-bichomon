2177fcf59c08e4558e1bbff249f59a84607bd910
Ainhoa Colorado <ainhoa.colorado.art@gmail.com>
2025-03-28 20:42:01 +0100

    first commit

--- scripts.js
+++ scripts.js
@@ -1,1 +1,20 @@
 console.log(document.title);
+let value
+value = document
+console.log(value)
+
+
+document.getElementById('gen-1')
+
+const titulo = document.getElementById('gen-1')
+
+
+
+
+titulo.innerText = 'Generasión 1 Pokimon'
+
+console.log(titulo)
+
+
+
+'gen-1'
